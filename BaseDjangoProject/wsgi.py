7103838dc78342e7399f08d01341173ae689bcb8
Sumit <onlysumitg@gmail.com>
2021-10-16 12:49:26 -0500

    Restored project name

--- BaseDjangoProject/wsgi.py
+++ BaseDjangoProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NewProject project.
+WSGI config for BaseDjangoProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BaseDjangoProject.settings')
 
 application = get_wsgi_application()
